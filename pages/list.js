d4d7739b359182c421442ac8699382822134268f
王冬 <wangdong@administratordeMacBook-Pro.local>
2020-10-14 19:42:55 +0800

    跳转页面

--- pages/list.js
+++ pages/list.js
@@ -1,34 +1,33 @@
+import Link from "next/link";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Link from 'next/link'
 
-export default function Home() {
+export default function List() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>index</title>
+        <title>list</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <header className={styles.header}>
-        <div>index title</div>
+        <div>list title</div>
       </header>
 
       <main className={styles.main}>
-        <div>index</div>
+        <div>list</div>
         <div>
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </div>
-
-        <div>
-          <Link href="/list">
+          <Link href="/">
             <button>this page!</button>
           </Link>
+          <a href="/">
+            <button>a链接this page!</button>
+          </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <div>index footer</div>
+        <div>list footer</div>
       </footer>
     </div>
   );
